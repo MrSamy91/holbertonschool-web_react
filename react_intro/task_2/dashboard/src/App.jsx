2d8732f3c0caf2dcc061cb4d31a10b1f948490ab
Samy <samy.ajouid@gmail.com>
2026-02-07 13:23:31 +0100

    fix task 2 exact format

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -4,7 +4,7 @@ import logo from './assets/holberton-logo.jpg';
 import Notifications from './Notifications';
 import { getCurrentYear, getFooterCopy } from './utils';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="root-notifications">
@@ -12,20 +12,22 @@ function App() {
       </div>
 
       <header className="App-header">
-        <img src={logo} alt="holberton logo" />
+        <img src={logo} alt="holberton logo" className="logo" />
         <h1>School dashboard</h1>
       </header>
 
       <main className="App-body">
         <p>Login to access the full dashboard</p>
 
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
+        <form className="Login-form" onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
 
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
 
-        <button>OK</button>
+          <button type="submit">OK</button>
+        </form>
       </main>
 
       <footer className="App-footer">
@@ -38,5 +40,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
