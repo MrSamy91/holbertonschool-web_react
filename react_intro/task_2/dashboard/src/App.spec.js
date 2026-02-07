373283e35b46d0393125e587d9d3430d531004ab
Samy <samy.ajouid@gmail.com>
2026-02-07 13:28:19 +0100

    add task 2 form tests

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -30,4 +30,22 @@ describe('App', () => {
       screen.getByRole('img', { name: /holberton logo/i })
     ).toBeInTheDocument();
   });
+
+  test('renders two input elements (email and password)', () => {
+    const { container } = render(<App />);
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(container.querySelectorAll('input')).toHaveLength(2);
+  });
+
+  test('renders two labels with texts "Email" and "Password"', () => {
+    render(<App />);
+    expect(screen.getByText(/email/i).tagName).toBe('LABEL');
+    expect(screen.getByText(/password/i).tagName).toBe('LABEL');
+  });
+
+  test('renders a button with text OK', () => {
+    render(<App />);
+    expect(screen.getByRole('button', { name: /ok/i })).toBeInTheDocument();
+  });
 });
